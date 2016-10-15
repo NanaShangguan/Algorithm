20b32d0f47ea3d067a60717c4e31577aedeb485f
Luo <qlluo@amazon.com>
2016-10-15 23:12:28 +0800

    Indeed problem 4

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by nana_ on 2016/8/28.
@@ -14,31 +11,18 @@ public class Main {
         PrintStream out = System.out;
 
         int n = in.nextInt();
-        double[][] s = new double[n + 1][n + 1];
-        s[1][1] = 1.0;
-        s[2][1] = 1.0;
-        s[3][1] = 1.0 / 3.0;
-        s[3][2] = 2.0 / 3.0;
+        double[] dp = new double[n + 1];
 
-        for (int i = 4; i <= n; i++) {
-            //1 last
-            for (int a = 0; a <= i - 2; a++)
-                if (s[i - 2][a] != 0) {
-                    s[i][a + 1] = (1.0 / i) * s[i - 2][a] * 2;
-                }
-            //middle
+        dp[1] = 1.0; dp[2] = 1.0;
+        for (int i = 3; i <= n; i++) {
+            dp[i] += (dp[i - 2] + 1) * (1.0 / i) * 2;
             int mid = i / 2;
-            for (int p = 3; p <= mid; p++) {
-                for (int a = 0; a <= p - 2; a++)
-                    for (int b = 0; b <= i - p - 1; b++)
-                        if (s[p - 2][a] != 0 && s[i - p - 1][b] != 0) {
-                            s[i][a + b + 1] += s[p - 2][a] * s[i - p - 1][b] * (1.0 / i) * 2;
-                        }
-            }
+            for (int j = 2; j <= mid; j++)
+                dp[i] += (dp[j - 2] + 1 + dp[i - j - 1]) * (1.0 / i) * 2;
+            if ((i & 1) == 1)
+                dp[i] += (dp[mid - 1] + 1 + dp[i - mid - 2]) * (1.0 / i);
         }
-        double exp = 0;
-        for (int i = 1; i <= n; i++)
-            exp += s[n][i] * i;
-        out.println(exp);
+
+        out.println(dp[n]);
     }
 }
